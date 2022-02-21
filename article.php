06838b584bcdba931bc64ddb6547692009e0546b
RatanovOV <ratanovov@hq.tatneft.ru>
2022-02-21 15:55:11 +0300

    add: article.php (добавление статьи), вывод всех новостей на главной

---
+++ article.php
@@ -0,0 +1,71 @@
+<?php
+    if(!isset($_COOKIE['login'])){
+        header('Location: /reg.php');
+        exit();
+    }
+?>
+<!doctype html>
+<html lang="en">
+<head>
+    <?php
+    $website_title = 'Добавление статьи';
+    require 'layout/head.php';
+    ?>
+</head>
+<body>
+    <?php require 'layout/header.php'; ?>
+
+    <main class="container mt-5">
+        <div class="row">
+            <div class="col-8">
+                <h4>Добавление статьи</h4>
+                <form action="" method="post">
+                    <label for="title">Заголовок статьи</label>
+                    <input type="text" name="title" id="title" class="form-control">
+
+                    <label for="intro">Интро статьи</label>
+                    <textarea name="intro" id="intro" class="form-control"></textarea>
+
+                    <label for="text">Текст статьи</label>
+                    <textarea name="text" id="text" class="form-control"></textarea>
+
+                    <div class="alert alert-danger mt-3" id="errorBlock"></div>
+
+                    <button type="button" class="btn btn-success mt-3" id="article_btn">Добавить</button>
+                </form>
+            </div>
+
+            <?php require 'layout/aside.php'; ?>
+        </div>
+    </main>
+    <?php require 'layout/footer.php'; ?>
+
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js"></script>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
+    <script>
+        $('#article_btn').click(function () {
+            const title = $('#title').val();
+            const intro = $('#intro').val();
+            const text = $('#text').val();
+
+            $.ajax({
+                url: 'reg/add_article.php',
+                type: 'POST',
+                cache: false,
+                data: {'title': title, 'intro': intro, 'text': text},
+                dataType: 'html',
+                success: function (data) {
+                    if(data == "Готово") {
+                        $('#article_btn').text('Все готово').attr('disabled','disabled');
+                        $('#errorBlock').hide();
+                    }
+                    else {
+                        $('#errorBlock').text(data).show();
+                    }
+                }
+            });
+        })
+    </script>
+</body>
+</html>
--- index.php
+++ index.php
@@ -1,23 +1,39 @@
 <!doctype html>
 <html lang="en">
-<head>
-    <?php
+    <head>
+        <?php
         $website_title = 'Блог';
-        require 'layouts/head.php';
-    ?>
-</head>
-<body>
-    <?php require 'layouts/header.php'; ?>
+        require 'layout/head.php';
+        ?>
+    </head>
+    <body>
+    <?php require 'layout/header.php'; ?>
+
     <main class="container mt-5">
         <div class="row">
-            <div class="col-8">Основная часть</div>
+            <div class="col-8">
+                <!--Основная часть-->
+                <?php
+                require_once 'mysql_connect.php';
 
-            <?php require 'layouts/aside.php'; ?>
+                $sql = 'SELECT * FROM `articles` ORDER BY `date` DESC';
+                $query = $pdo->query($sql);
+                while ($row = $query->fetch(PDO::FETCH_OBJ)) {
+                    echo "<h2>$row->title</h2>
+                                  <p>$row->intro</p>
+                                  <p><b>Автор статьи: </b> <mark>$row->author</mark></p>
+                                  <a href='/news.php?id=$row->id' title='$row->title'>
+                                  <button class='btn btn-info mb-5'>Подробнее</button>
+                                  </a>";
+                }
+                ?>
+            </div>
+            <?php require 'layout/aside.php'; ?>
         </div>
     </main>
-    <?php require 'layouts/footer.php'; ?>
+    <?php require 'layout/footer.php'; ?>
 
 
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js"></script>
-</body>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+    </body>
 </html>
